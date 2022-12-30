6aa4d8ea825fe788823ad40b4c335dcc508e54f7
Mehmet Uysal <mehmetuysal2000@yandex.com>
2022-12-30 22:39:47 +0300

    expressTs
    I dont now  what I did

--- configuration/corsConfiguration.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  origin: ["https://mehmetuysal.tech", /h?t?t?p?s?localhost:[0-9]+/],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: true,
-  optionsSuccessStatus: 100,
-};
